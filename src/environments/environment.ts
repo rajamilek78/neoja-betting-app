e44588973711b3070376ccf02c5ef785a9ce3b10
Shraddha-Bhad16 <shraddha.bhad@amilekconsultancy.com>
2024-06-06 17:20:01 +0530

    all style related changes updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   demo: false,
   dev: false,
-  apiUrl: 'http://192.168.1.3:8282',
+  apiUrl: 'http://192.168.31.222:8282',
   // apiUrl: 'http://localhost:3000',
-
 };

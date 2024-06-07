5cb9e6cc11ac05710c83c3b0fba01d017bbd612b
Shraddha-Bhad16 <shraddha.bhad@amilekconsultancy.com>
2024-06-07 18:15:50 +0530

    changes updated

--- src/app/private-modules/operator-module/components/operator/operator.component.ts
+++ src/app/private-modules/operator-module/components/operator/operator.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   CdkDragDrop,
   CdkDrag,
@@ -20,7 +27,7 @@ import { DOCUMENT } from '@angular/common';
 export class OperatorComponent implements OnInit {
   isWindowScrolledToBottom: boolean = false;
   @ViewChild('playerInput') playerInput!: ElementRef;
-  coppyteamPlayer:{name:string}[]=[];
+  coppyteamPlayer: { name: string }[] = [];
   teamPlayer: { name: string }[] = [];
   blueTeamPlayers: { name: string }[] = [];
   redTeamplayers: { name: string }[] = [];
@@ -76,21 +83,20 @@ export class OperatorComponent implements OnInit {
     }, 0);
   }
 
-
-
-@HostListener('window:scroll', [])
-onWindowScroll() {
-  if (document.body.scrollTop > 200 ||     
-  document.documentElement.scrollTop > 200) {
-    document.getElementById('playerList')!.classList.add('scroll-length');
-    document.getElementById('playerList')!.classList.remove('operator__scroll');
-    //document.getElementById('').classList.add('green');
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      document.getElementById('playerList')!.classList.add('scroll-length');
+      // document.getElementById('playerList')!.classList.remove('operator__scroll');
+      //document.getElementById('').classList.add('green');
+    } else {
+      document.getElementById('playerList')!.classList.remove('scroll-length');
+      // document.getElementById('playerList')!.classList.add('operator__scroll');
+    }
   }
-  else{
-    document.getElementById('playerList')!.classList.remove('scroll-length');
-    document.getElementById('playerList')!.classList.add('operator__scroll');
-  }
-}
 
   // ngAfterViewInit() {
   //   const playerList = document.getElementById('operatorScroll');
@@ -124,24 +130,24 @@ onWindowScroll() {
             this.coppyteamPlayer.push(payload);
             this.savePlayersToLocalStorage();
             setTimeout(() => {
-              const operatorScroll = document.querySelector('.operator__scroll');
+              const operatorScroll =
+                document.querySelector('.operator__scroll');
               const scrollLength = document.querySelector('.scroll-length');
               let playerList;
-              
+
               if (operatorScroll) {
                 playerList = operatorScroll;
               } else if (scrollLength) {
                 playerList = scrollLength;
               }
-              
+
               if (playerList) {
                 const lastPlayer = playerList.lastElementChild;
-              
+
                 if (lastPlayer) {
                   // Scroll to the last player
                   lastPlayer.scrollIntoView({ behavior: 'smooth' });
                 }
-              
               }
             }, 0);
           },
@@ -170,7 +176,7 @@ onWindowScroll() {
           next: (res: any) => {
             console.log(res);
             this.teamPlayer[index].name = newName;
-            this.coppyteamPlayer[index].name = newName
+            this.coppyteamPlayer[index].name = newName;
             this.savePlayersToLocalStorage();
           },
           error: (err: any) => {
@@ -316,7 +322,7 @@ onWindowScroll() {
 
   startNewGame() {
     this.loadPlayersFromLocalStorage();
-    this.isDragDropDisabled = false
+    this.isDragDropDisabled = false;
     this.blueTeamPlayers = [];
     this.redTeamplayers = [];
     this.blueTeamBatter = [];
@@ -352,7 +358,7 @@ onWindowScroll() {
     localStorage.removeItem('teamPlayers');
     // this.CommonService.resetDatabase().subscribe(
     //   (res) => {
-      
+
     //     console.log(res);
     //   },
     //   (err) => {
